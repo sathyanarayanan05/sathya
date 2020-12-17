896da172915b73ff038e19de47b04adc5a254d0a
House <sathyaon98@gmail.com>
2020-12-17 17:13:53 +0530

    hello

--- src/dem/hello.java
+++ src/dem/hello.java
@@ -3,8 +3,11 @@ package dem;
 public class hello {
 
 	public void show() {
+		public static void main(String[] args) {
+			
 		
-		System.out.println("Hello, World!");
+		
+		System.out.println("Hello, World!");\
 	}
 
 }
